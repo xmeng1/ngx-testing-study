62d5dbca1e906727371cabeedc1f7ba434a021dc
Xin Meng <mengxin.city@gmail.com>
2017-11-28 12:41:17 +0000

    TestBed fixture detection change
    1. spy service method to change return and detect change to verify element in view

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,18 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { AuthService } from '../auth.service';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let authService: AuthService;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
+      declarations: [LoginComponent],
       providers: [AuthService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,10 +24,13 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     // get test component from the fixture
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
 
     // UserService provided to the TestBed
     authService = TestBed.get(AuthService);
+
+    // get the "a" element by CSS selector (e.g., by class name)
+    el = fixture.debugElement.query(By.css('a'));
   });
 
   it('should create', () => {
@@ -45,4 +51,22 @@ describe('LoginComponent', () => {
     expect(authService.isAuthenticated).toHaveBeenCalled();
   });
 
+
+  it('login button hidden when the user is authenticated', () => {
+    // TODO
+    // To being with Angular has not done any change detection so the content is blank.
+    expect(el.nativeElement.textContent.trim()).toBe('');
+    // Trigger change detection and this lets the template update to the initial value which is Login since by
+    // default we are not authenticated
+    fixture.detectChanges();
+    expect(el.nativeElement.textContent.trim()).toBe('Login');
+    // Change the authentication state to true
+    spyOn(authService, 'isAuthenticated').and.returnValue(true);
+    // The label is still Login! We need changeDetection to run and for angular to update the template.
+    expect(el.nativeElement.textContent.trim()).toBe('Login');
+    // Which we can trigger via fixture.detectChange()
+    fixture.detectChanges();
+    // Now the label is Logout
+    expect(el.nativeElement.textContent.trim()).toBe('Logout');
+  });
 });
