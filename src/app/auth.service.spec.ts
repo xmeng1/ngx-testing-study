6061d4b9abf563f98de8ab7b51f7bd6ded25b077
Xin Meng <mengxin.city@gmail.com>
2017-11-28 12:02:28 +0000

    test service by using injection

---
+++ src/app/auth.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AuthService]
+    });
+  });
+
+  afterEach(() => {
+    localStorage.removeItem('token');
+  });
+
+  it('should be created', inject([AuthService], (service: AuthService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('should return true from isAuthenticated when there is a token',
+      inject([AuthService], (service: AuthService) => {
+    localStorage.setItem('token', '1234');
+    expect(service.isAuthenticated()).toBeTruthy();
+  }));
+
+  it('should return false from isAuthenticated when there is no token',
+    inject([AuthService], (service: AuthService) => {
+      expect(service.isAuthenticated()).toBeFalsy();
+    }));
+
+});
---
+++ src/app/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
