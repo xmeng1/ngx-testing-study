a410345b2925ccc26982e16e4954502d01f465c9
Xin Meng <mengxin.city@gmail.com>
2017-11-28 12:10:08 +0000

    add pipe test which need new instance
    1. test the transform function in the pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DefaultPipe } from './default.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultPipe
   ],
   imports: [
     BrowserModule
