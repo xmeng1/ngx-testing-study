4b9ff4cb75e7ff7ab414f333e1617f96103d3420
Xin Meng <mengxin.city@gmail.com>
2017-11-28 12:28:41 +0000

    TestBed for component and service
    1. spy the service

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../auth.service';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,12 +17,32 @@ describe('LoginComponent', () => {
   }));
 
   beforeEach(() => {
+    // create component and test fixture
     fixture = TestBed.createComponent(LoginComponent);
+    // get test component from the fixture
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    // UserService provided to the TestBed
+    authService = TestBed.get(AuthService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('needsLogin returns true when the user is not authenticated', () => {
+    // spy service and mock the method to return some value
+    spyOn(authService, 'isAuthenticated').and.returnValue(false);
+    expect(component.needsLogin()).toBeTruthy();
+    // spy service to verify whether method is executed
+    expect(authService.isAuthenticated).toHaveBeenCalled();
+  });
+
+  it('needsLogin returns false when the user is authenticated', () => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(true);
+    expect(component.needsLogin()).toBeFalsy();
+    expect(authService.isAuthenticated).toHaveBeenCalled();
+  });
+
 });
