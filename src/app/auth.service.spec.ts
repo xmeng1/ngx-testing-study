58bb46bab746c07388b855190beeb8e441f50a5b
Xin Meng <mengxin.city@gmail.com>
2018-01-19 11:41:14 +0000

    assertion for Promise
    https://stackoverflow.com/questions/33023505/karma-mock-promise-response

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
 
 describe('AuthService', () => {
   beforeEach(() => {
@@ -18,14 +18,20 @@ describe('AuthService', () => {
   }));
 
   it('should return true from isAuthenticated when there is a token',
-      inject([AuthService], (service: AuthService) => {
-    localStorage.setItem('token', '1234');
-    expect(service.isAuthenticated()).toBeTruthy();
-  }));
+    inject([AuthService], (service: AuthService) => {
+      localStorage.setItem('token', '1234');
+      expect(service.isAuthenticated()).toBeTruthy();
+    }));
 
   it('should return false from isAuthenticated when there is no token',
     inject([AuthService], (service: AuthService) => {
-      expect(service.isAuthenticated()).toBeFalsy();
+      localStorage.setItem('token1', '1234');
+      let responseData: any;
+      service.isAuthenticated().then(function (data) {
+        responseData = data;
+        expect(responseData).toBeFalsy();
+      });
+
     }));
 
 });
